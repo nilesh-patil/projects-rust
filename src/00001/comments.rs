0d8e7f566b23dc99f2260b18492a69767290376c
Nilesh <nilesh-patil@users.noreply.github.com>
2025-03-28 04:54:51 +0530

    RS00001 : Create Comments.rs
    Just file for comments variations

---
+++ src/00001/comments.rs
@@ -0,0 +1,10 @@
+fn main() {
+    /*
+    Adding comments to your code is a good practice.
+    Comments are ignored by the compiler and are not executed. 
+    */
+    let l = 8;
+    let w = 5;
+    let area = l * w;
+    println!("The area of the rectangle is {}", area);
+}
